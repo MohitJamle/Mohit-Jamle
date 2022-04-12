45ce66bef52095cf8c7487e5b72432398ee04a4d
Mohit jamle <bhojrajjamle@gmail.com>
2022-04-12 20:51:56 +0530

    Fourth commit

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,44 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    // console.log(event.target);
+
+    // emailjs
+    //   .sendForm(
+    //     "service_8fitjgj",
+    //     "template_exqjahr",
+    //     event.target,
+    //     "l3IE4446-Zjbr8VyL"
+    //   )
+    //   .then((res) => console.log(res))
+    //   .catch((error) => console.log(error));
+
+    setName("");
+    setEmail("");
+    setNumber("");
+    setMessage("");
+  };
+
   return (
     <div className="contact">
-      <form
-        className="contact_form"
-        action="mailto:mohitjamle111@gmail.com"
-        method="Post"
-        encType="text/plain"
-      >
+      <form className="contact_form" onSubmit={submitHandler}>
         <h1>Contact Us </h1>
         <div className="inputs">
           <input
             type="text"
             id="name"
-            name="UserName"
+            name="user_name"
             placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
           />
           <input
             type="email"
             id="email"
-            name="UserEmail"
+            name="user_email"
             placeholder="example@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
           <input
             type="number"
             id="number"
-            name="Number"
+            name="user_number"
             placeholder="mobile number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
             required
           />
         </div>
         <textarea
-          name="message"
+          name="user_message"
           id="message"
           rows={10}
           placeholder="Type your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           required
         ></textarea>
         <button type="submit">Send Message</button>
